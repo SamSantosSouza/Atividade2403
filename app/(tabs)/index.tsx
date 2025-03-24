0b5030e01a883919a7a0da7cf3b3515e61cd061a
SamSantosSouza <santossamuell1235@gmail.com>
2025-03-24 11:01:40 -0300

    feat: alterando o texto

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,17 +9,18 @@ import { View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View>
-    <Text> Oh denise sai da live filha</Text>
+    <Text> Oh denise! sai da live filha ninguém te chamou aqui caralho!!
+    </Text>
     <Image 
       style={{ width: 300 , height: 300 }}
-      source={{uri: 'https://s2.glbimg.com/O23UOPFNHW0wBG4O3OTgECObi_Q=/0x0:620x722/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2022/G/B/ZAPKVkS5mlEdst0i8qzQ/2019-07-20-fernanda.jpg'}}
+      source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlq4cbufw6RSTbXBSZ9WQXFcbX6CTV-dGnxw&s'}}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textStyle: {
+ View: {
     backgroundColor: 'white',
   },
 });
